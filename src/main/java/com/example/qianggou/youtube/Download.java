4d2218fcc69c3b089ade29f65a3c8bf9d9a9ab6b
杨光义 <yangguangyi@gridsum.com>
2020-11-06 17:59:01 +0800

    提交代码

---
+++ src/main/java/com/example/qianggou/youtube/Download.java
@@ -0,0 +1,144 @@
+package com.example.qianggou.youtube;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.LineHandler;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PrimitiveIterator;
+import java.util.stream.Collectors;
+
+/**
+ * @Description
+ * @Author ygy
+ * @Date 2020/11/6
+ */
+public class Download {
+
+    /**
+     * youtube搜索后页面url
+     */
+    private static final String YOUTUBE_URL="https://www.youtube.com/results?search_query=%E4%B8%AD%E6%96%87asmr";
+
+    private static final String DOWNLOAD_URL="https://www.youtubeconverter.io/en6";
+    /**
+     * chromedriver路径
+     */
+    private static final String CHROME_DRIVER_PATH = "C:\\Users\\T480\\Desktop\\chromedriver.exe";
+    /**
+     * url存储路径
+     */
+    private static final String URL_FILE_PATH="C:\\Users\\T480\\Desktop\\url.txt";
+
+    private static final String FAIL_FILE_PATH="C:\\Users\\T480\\Desktop\\failurl.txt";
+    /**
+     * 爬取最大数量
+     */
+    private static final int COUNT=100;
+    /**
+     * 爬取起始值
+     */
+    private static final int START=0;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+        driver.get(YOUTUBE_URL);
+        getUrl(driver);
+        download(driver);
+    }
+
+    private static void download(WebDriver driver) throws InterruptedException, IOException {
+        List<String> urls=FileUtil.readUtf8Lines(URL_FILE_PATH);
+        System.out.println("urls.size()="+urls.size());
+        driver.get(DOWNLOAD_URL);
+        List<String> failList=new ArrayList<>();
+        for (int i=START;i<urls.size();i++){
+            if (urls.get(i)==null || "".equals(urls.get(i))){
+                continue;
+            }
+            while (true){
+                if (isJudgingElement(driver,By.id("ytUrl"))){
+                    break;
+                }
+            }
+            driver.findElement(By.id("ytUrl")).sendKeys(urls.get(i));
+            driver.findElement(By.id("convertBtn")).click();
+            while (true){
+                if (isJudgingElement(driver,By.linkText("Download"))){
+                    break;
+                }
+            }
+            List<WebElement> webElements=driver.findElements(By.linkText("Download"));
+            List<WebElement> webElementList=webElements.stream().filter(webElement->webElement.getAttribute("onclick").contains("720p")).collect(Collectors.toList());
+            if (webElementList.isEmpty()){
+                webElementList=webElements.stream().filter(webElement->webElement.getAttribute("onclick").contains("360p")).collect(Collectors.toList());
+                if (webElementList.isEmpty()){
+                    webElementList=webElements.stream().filter(webElement->webElement.getAttribute("onclick").contains("240p")).collect(Collectors.toList());
+                }
+            }
+            if (!webElementList.isEmpty()){
+                webElementList.get(0).click();
+            } else {
+                failList.add(urls.get(i));
+                continue;
+            }
+            while (true){
+                if (isJudgingElement(driver,By.linkText("Convert next"))){
+                    break;
+                }
+            }
+            String downloadHref=driver.findElement(By.linkText("Download")).getAttribute("href");
+            driver.navigate().to(downloadHref);
+            Thread.sleep(1000);
+            driver.navigate().back();
+        }
+        FileUtil.writeUtf8Lines(failList,FAIL_FILE_PATH);
+    }
+
+    private static void getUrl(WebDriver driver) throws IOException {
+        File file=new File(URL_FILE_PATH);
+        FileWriter fileWriter=new FileWriter(file);
+        BufferedWriter bw= new BufferedWriter(fileWriter);
+        try {
+            List<WebElement> webElements=driver.findElements(By.xpath("//a[@id='video-title']"));
+            //模拟滚轮一直向下滑动加载
+            Actions actions=new Actions(driver);
+            while(webElements.size()<=COUNT){
+                actions.sendKeys(Keys.DOWN).perform();
+                Thread.sleep(50);
+                webElements=driver.findElements(By.xpath("//a[@id='video-title']"));
+            }
+
+            for (int i=START;i<webElements.size();i++){
+                String href=webElements.get(i).getAttribute("href");
+                System.out.println(href);
+                bw.write(href);
+                bw.newLine();
+            }
+
+            System.out.println("总过读取到"+webElements.size()+"条");
+            System.out.println("写入"+(webElements.size()-START)+"条");
+            bw.close();
+        } catch (Exception e){
+            System.out.println("没有找到符合条件的标签");
+        }
+    }
+
+    private static boolean isJudgingElement(WebDriver webDriver, By by) {
+        try {
+            webDriver.findElement(by);
+            return true;
+        } catch (Exception e) {
+            //System.out.println("不存在此元素");
+            return false;
+        }
+    }
+}
